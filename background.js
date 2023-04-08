f15f620ef0077f0311a0577af00819772b73495a
Franck Dupont <franck.dupont.w@gmail.com>
2023-04-08 19:20:14 +0200

    add turn off turn on

--- background.js
+++ background.js
@@ -1,10 +1,40 @@
-let url = chrome.runtime.getURL('tone.wav')
+let isExtensionOn = true;
+chrome.browserAction.setIcon({path: "awake-logo-on128.png"});
+chrome.browserAction.setIcon({ path: { "16": "awake-logo-on16.png",
+                                       "32": "awake-logo-on32.png",
+                                       "48": "awake-logo-on48.png",
+                                   	   "128": "awake-logo-on128.png"} });
+let timeoutID;
 
-let a = new Audio(url)
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if(isExtensionOn) {
+    isExtensionOn = false;
+    chrome.browserAction.setIcon({ path: { "16": "awake-logo-off16.png",
+                                       "32": "awake-logo-off32.png",
+                                       "48": "awake-logo-off48.png",
+                                   	   "128": "awake-logo-off128.png"} });
+    clearTimeout(timeoutID);
+  } else {
+    isExtensionOn = true;
+    chrome.browserAction.setIcon({ path: { "16": "awake-logo-on16.png",
+                                       "32": "awake-logo-on32.png",
+                                       "48": "awake-logo-on48.png",
+                                   	   "128": "awake-logo-on128.png"} });
+    playSound();
+  }
+});
 
-a.play()
+function playSound() {
+  if(isExtensionOn) {
+    let url = chrome.runtime.getURL('tone.wav')
+    let a = new Audio(url)
+    a.play()
+    timeoutID = setTimeout(function(){
+        a.pause();
+        a.currentTime = 0;
+        playSound();
+    }, 600000) // 10min
+  }
+}
 
-setInterval(function(){
-	a.currentTime = 0;
-	a.play()
-}, 600000) // 10min
+playSound();
