1018903a75759d85ad861c1079b81da60eeb13d9
Franck Dupont <franck.dupont.w@gmail.com>
2023-04-08 19:28:35 +0200

    rewrite

--- background.js
+++ background.js
@@ -1,25 +1,24 @@
 let isExtensionOn = true;
-chrome.browserAction.setIcon({path: "awake-logo-on128.png"});
-chrome.browserAction.setIcon({ path: { "16": "awake-logo-on16.png",
-                                       "32": "awake-logo-on32.png",
-                                       "48": "awake-logo-on48.png",
-                                   	   "128": "awake-logo-on128.png"} });
+chrome.browserAction.setIcon({ path: { "16": "/images/awake-logo-on16.png",
+                                       "32": "/images/awake-logo-on32.png",
+                                       "48": "/images/awake-logo-on48.png",
+                                   	   "128": "/images/awake-logo-on128.png"} });
 let timeoutID;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   if(isExtensionOn) {
     isExtensionOn = false;
-    chrome.browserAction.setIcon({ path: { "16": "awake-logo-off16.png",
-                                       "32": "awake-logo-off32.png",
-                                       "48": "awake-logo-off48.png",
-                                   	   "128": "awake-logo-off128.png"} });
+    chrome.browserAction.setIcon({ path: { "16": "/images/awake-logo-off16.png",
+                                       "32": "/images/awake-logo-off32.png",
+                                       "48": "/images/awake-logo-off48.png",
+                                   	   "128": "/images/awake-logo-off128.png"} });
     clearTimeout(timeoutID);
   } else {
     isExtensionOn = true;
-    chrome.browserAction.setIcon({ path: { "16": "awake-logo-on16.png",
-                                       "32": "awake-logo-on32.png",
-                                       "48": "awake-logo-on48.png",
-                                   	   "128": "awake-logo-on128.png"} });
+    chrome.browserAction.setIcon({ path: { "16": "/images/awake-logo-on16.png",
+                                       "32": "/images/awake-logo-on32.png",
+                                       "48": "/images/awake-logo-on48.png",
+                                   	   "128": "/images/awake-logo-on128.png"} });
     playSound();
   }
 });
